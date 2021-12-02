b50165f06c9e2d5c14d8efdc903b1500b85d703f
Danoush <67105391+dhoseini@users.noreply.github.com>
2021-12-02 16:05:54 +0330

    Add files via upload

---
+++ Nugget/bapl.bat
@@ -0,0 +1,35 @@
+@ECHO OFF
+title BAPL SERVER SpilC.c1.biz
+echo Copyright 2011-2021 dhoseini And SpilC
+echo Take a Coffee. Sit Down and Relax
+adb shell input keyevent 26
+adb shell "input swipe 300 1000 300 300"
+adb shell sendevent /dev/input/event3 3 57 14
+
+adb shell sendevent /dev/input/event3 1 330 1
+
+adb shell sendevent /dev/input/event3 3 53 x1
+adb shell sendevent /dev/input/event3 3 54 y1
+adb shell sendevent /dev/input/event3 3 58 57
+adb shell sendevent /dev/input/event3 0 0 0
+
+adb shell sendevent /dev/input/event3 3 53 x2
+adb shell sendevent /dev/input/event3 3 54 y2
+adb shell sendevent /dev/input/event3 3 58 57
+adb shell sendevent /dev/input/event3 0 0 0
+
+adb shell sendevent /dev/input/event3 3 53 x3
+adb shell sendevent /dev/input/event3 3 54 y3
+adb shell sendevent /dev/input/event3 3 58 57
+adb shell sendevent /dev/input/event3 0 0 0
+adb shell sendevent /dev/input/event3 3 53 xn
+adb shell sendevent /dev/input/event3 3 54 yn
+adb shell sendevent /dev/input/event3 3 58 57
+adb shell sendevent /dev/input/event3 0 0 0
+
+adb shell sendevent /dev/input/event3 3 57 4294967295
+adb shell sendevent /dev/input/event3 1 330 0
+adb shell sendevent /dev/input/event3 0 0 0
+adb shell sendevent /dev/input/event3 1 330 1
+adb shell sendevent /dev/input/event3 1 330 0
+adb shell "input swipe 300 1000 300 300"
